dbb9c6e3b4c6ab8bf657c9ffcb665aa283e218e3
Jan Wennrich <jan.wennrich@pcsg.de>
2019-10-28 09:31:05 +0100

    feat: adds CookieCollection

---
+++ src/QUI/CookieConsent/CookieCollection.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @author PCSG (Jan Wennrich)
+ */
+
+namespace QUI\CookieConsent;
+
+
+use QUI\Collection;
+
+/**
+ * Class CookieCollection
+ *
+ * @package QUI\CookieConsent
+ */
+class CookieCollection extends Collection
+{
+    protected $allowed = [CookieInterface::class];
+}

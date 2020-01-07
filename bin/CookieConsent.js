d3218b0ef04626ea6089c25802bc76a35e34610f
Jan Wennrich <jan.wennrich@pcsg.de>
2020-01-07 11:08:52 +0100

    Reverts all new GDPR changes. Sets the branch to the state of branch/version 1.5.1
    This reverts commit 79f8a37fce23583f660ab7321463312287c67905.
    This reverts commit 6eea0ede2c8333d7d9c49ca08376c839e5a3c60d.
    This reverts commit 1d55df7a345012368531a113960bd16cad7fd03d.
    This reverts commit f54992824ffec587c2ef049321e8a1603bdfb1bb.
    This reverts commit 0795ab7141e04ca8280e78714c2e2b187f4d3dd8.
    This reverts commit 4271867f4917f9539427d662767fbab6f8b92eae.
    This reverts commit 973d85572296307e725771deaf664a446d3df06b.
    This reverts commit 30af60734a9f54e04242c0acdef3c74254bbff82.
    This reverts commit d82e630550421684426c1ba675627ef8d9d83efa.
    This reverts commit dda20cd6546c54ab2b3fa1cc2a4e35ff01cf09b6.
    This reverts commit c0b2354c18f749b9cdf9dfba4f5e36c317114868.
    This reverts commit af7baad699be8feb70c89f6fb8004146023fbe44.
    This reverts commit 2856231b6adcc2d946047cb7c19ada855705d6eb.
    This reverts commit 0f454c754b6a264bda41c4fd409272854c76f5af.
    This reverts commit bd5f8b6b80087f23151790f175640a4ccf82b525.
    This reverts commit bb8bbd0e6090559a59a78409d8d67ff370db94b0.
    This reverts commit 4ecd850dfd5c6acbc588b3ad73f3fd9bb71ec4cf.
    This reverts commit e7e33ab96b61054944fb901713b6a5bd94b81f8f.
    This reverts commit 3324ac42ffe3b1913f9505b73600fdb7523879b4.

---
+++ bin/CookieConsent.js
@@ -0,0 +1,106 @@
+/**
+ * @module package/quiqqer/cookieconsent/bin/CookieConsent
+ * @author www.pcsg.de (Henning Leutz)
+ */
+define('package/quiqqer/cookieconsent/bin/CookieConsent', [
+    'qui/QUI',
+    'Locale'
+], function (QUI, QUILocale) {
+    "use strict";
+
+    if (QUI.Storage.get('quiqqer-cookieconsent-accept')) {
+        return;
+    }
+
+    var lg = 'quiqqer/cookieconsent';
+
+    var accept = function () {
+        var Message = document.getElement('.quiqqer-cookieconsent');
+        var animate = {
+            opacity: 0
+        };
+
+        if (!Message) {
+            return;
+        }
+
+        if (Message.hasClass('quiqqer-cookieconsent__top')) {
+            animate.top = -30;
+        } else if (Message.hasClass('quiqqer-cookieconsent__bottom')) {
+            animate.bottom = -30;
+        }
+
+        moofx(Message).animate(animate, {
+            duration: 300,
+            callback: function () {
+                QUI.Storage.set('quiqqer-cookieconsent-accept', 1);
+                Message.destroy();
+            }
+        });
+    };
+
+    var load = function () {
+        require([
+            'css!package/quiqqer/cookieconsent/bin/CookieConsent.css'
+        ], function () {
+            var Message = new Element('div', {
+                'class': 'quiqqer-cookieconsent',
+                html   : '<div class="quiqqer-cookieconsent-message">' +
+                QUILocale.get(lg, 'text') +
+                '</div>' +
+                '<button class="quiqqer-cookieconsent-accept">' +
+                QUILocale.get(lg, 'button') +
+                '</button>',
+                styles : {
+                    opacity: 0
+                }
+            });
+
+            if (window.QUIQQER_CC_POS === 'topSlide') {
+                Message.setStyles({});
+                Message.inject(document.body, 'top');
+            } else {
+                Message.inject(document.body);
+            }
+
+
+            if (typeof window.QUIQQER_CC_LINK !== 'undefined') {
+                new Element('a', {
+                    href  : window.QUIQQER_CC_LINK,
+                    html  : QUILocale.get(lg, 'PrivacyPolicy.link.text'),
+                    styles: {
+                        marginLeft: 10
+                    }
+                }).inject(Message.getElement('.quiqqer-cookieconsent-message'));
+            }
+
+
+            switch (window.QUIQQER_CC_POS) {
+                case 'topSlide':
+                    Message.addClass('quiqqer-cookieconsent__topSlide');
+                    break;
+
+                case 'bottom':
+                    Message.addClass('quiqqer-cookieconsent__bottom');
+                    break;
+
+                default:
+                case 'top':
+                    Message.addClass('quiqqer-cookieconsent__top');
+            }
+
+
+            Message.getElement('.quiqqer-cookieconsent-accept').addEvent('click', accept);
+
+            moofx(Message).animate({
+                opacity: 1
+            });
+        });
+    };
+
+    if (document.readyState === 'complete') {
+        load.delay(2000);
+    } else {
+        window.addEvent('load', load);
+    }
+});
---
+++ src/QUI/CookieConsent/EventHandler.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace QUI\CookieConsent;
+
+use QUI;
+
+/**
+ * Class EventHandler
+ *
+ * @package QUI\CookieConsent
+ */
+class EventHandler
+{
+    /**
+     * @param QUI\Template $Template
+     * @param QUI\Projects\Site $Site
+     */
+    public static function onTemplateSiteFetch($Template, $Site)
+    {
+        $Project = $Site->getProject();
+
+        // CookieConsent Data
+        $position = 'top';
+
+        switch ($Project->getConfig('quiqqer.cookieconsent.position')) {
+            case 'top':
+            case 'bottom':
+            case 'topSlide':
+                $position = $Project->getConfig('quiqqer.cookieconsent.position');
+                break;
+        }
+
+        $Template->extendFooter(
+            '<script>'.
+            'var QUIQQER_CC_POS = "'.$position.'";'.
+            'require(["package/quiqqer/cookieconsent/bin/CookieConsent"]);'.
+            '</script>'
+        );
+
+
+        // PrivacyPolicy Site
+        $list = $Project->getSites([
+            'where' => [
+                'type' => 'quiqqer/sitetypes:types/privacypolicy'
+            ],
+            'limit' => 1
+        ]);
+
+        if (isset($list[0])) {
+            try {
+                /* @var $PrivacyPolicy QUI\Projects\Site */
+                $PrivacyPolicy = $list[0];
+
+                $Template->extendFooter(
+                    '<script>var QUIQQER_CC_LINK = "'.$PrivacyPolicy->getUrlRewritten().'";</script>'
+                );
+            } catch (QUI\Exception $Exception) {
+            }
+        }
+    }
+
+    /**
+     * @param QUI\Template $Template
+     */
+    public static function onTemplateGetHeader($Template)
+    {
+        try {
+            $lastUpdate = QUI::getPackageManager()->getLastUpdateDate();
+            $lastUpdate = \md5($lastUpdate);
+        } catch (QUI\Exception $Exception) {
+            return;
+        }
+
+        $Template->extendHeader(
+            '<link rel="preload" as="style" href="'.URL_OPT_DIR.'quiqqer/cookieconsent/bin/CookieConsent.css?update='.$lastUpdate.'">'
+        );
+    }
+
+
+    public static function onPackageSetup(QUI\Package\Package $Package)
+    {
+        if ($Package->getName() != 'quiqqer/cookieconsent') {
+            return;
+        }
+
+        QUI\System\Log::write(
+            'The module "quiqqer/cookieconsent" is deprecated/abandoned. To comply with modern GDPR-guidelines you should use "quiqqer/gdpr" instead.',
+            QUI\System\Log::LEVEL_NOTICE
+        );
+    }
+}
